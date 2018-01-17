10c28ea654835ba00fe5e5e9b6d4d1a3caa9881f
vogdb <vozhdb@gmail.com>
2018-01-17 10:49:09 +0300

    Afferents transform to NEST format

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='moraud2016-spinal-cord-nest',
+    version='0.0.1',
+    description='a putative spinal cord neural network',
+    url='https://github.com/vogdb/moraud2016-spinal-cord-nest/',
+    license='MIT',
+    package_data={'spinal_cord': ['spinal_cord/afferents_data']}
+)
