bedd99f96c418cf89160f67ef0cef88eb5eace22
vogdb <vozhdb@gmail.com>
2018-03-18 15:28:51 +0300

    remove redundant multimeter plots

--- spinal_cord/display_results.py
+++ spinal_cord/display_results.py
@@ -7,7 +7,7 @@ from spinal_cord.network_items_names import Multimeters, SpikeDetectors
 import spinal_cord.params as spinal_cord_params
 from spinal_cord.device_plot import MultimeterPlot, SpikeDetectorPlot
 
-multimeter_plot = MultimeterPlot(5, 'Average "V_m" of neuron groups')
+multimeter_plot = MultimeterPlot(3, 'Average "V_m" of neuron groups')
 spike_detector_plot = SpikeDetectorPlot(5, 'Spike plots')
 
 
@@ -47,8 +47,6 @@ def display_results():
     plot_multimeter(Multimeters.FLEX_MOTOR, Multimeters.EXTENS_MOTOR, 'Motor')
     plot_multimeter(Multimeters.FLEX_INTER_1A, Multimeters.EXTENS_INTER_1A, 'Inter1A')
     plot_multimeter(Multimeters.FLEX_INTER_2, Multimeters.EXTENS_INTER_2, 'Inter2')
-    plot_multimeter(Multimeters.FLEX_AFFERENT_1A, Multimeters.EXTENS_AFFERENT_1A, 'Afferent1A')
-    plot_multimeter(Multimeters.FLEX_AFFERENT_2, Multimeters.EXTENS_AFFERENT_2, 'Afferent2')
 
     spike_detector_plot.reset()
     plot_spike_detector(SpikeDetectors.FLEX_MOTOR, SpikeDetectors.EXTENS_MOTOR, 'Motor',
